146b72ed8367a6d72a62ac0e4150dd34c67c8029
dianahorban <dianahorban477@gmail.com>
2026-04-03 16:38:13 +0200

    The solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,11 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cash = []
+    def wrapper(*args, **kwargs) -> None:
+        if args or kwargs in cash:
+            print ("Calculating new result")
+        else:
+            cash.append((args, kwargs))
+            print ("Getting from cache")
+        return wrapper
